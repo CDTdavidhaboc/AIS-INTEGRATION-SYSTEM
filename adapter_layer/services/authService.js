24db409c2df6245d9ba021aa14727002e05cab3e
CDTdavidhaboc <23-14890@g.batstate-u.edu.ph>
2026-04-21 16:52:55 +0800

    AUTH-ADAPTER-LEGACY

---
+++ adapter_layer/services/authService.js
@@ -0,0 +1,15 @@
+import * as AuthAdapter from '../adapters/authAdapter.js';
+
+export const registerStudent = async (studentProfile) => {
+    if(studentProfile.firstName === '' ||
+        studentProfile.lastName === '' ||
+        studentProfile.dob === '' || 
+        studentProfile.course === '' || 
+        studentProfile.major === '' ||
+        studentProfile.address === '' || 
+        studentProfile.status === ''){
+        throw new Error('All fields are required');
+    } 
+
+    return await AuthAdapter.create(studentProfile);
+}
